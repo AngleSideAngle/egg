74996ac0b051054ff4ed9061cf59839e7d8e21ca
Asa Paparo <asapaparo@gmail.com>
2022-10-30 22:47:10 -0400

    added particles

--- Assets/Scripts/GameBehavior.cs
+++ Assets/Scripts/GameBehavior.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class GameBehavior : MonoBehaviour
 {
     [SerializeField] int maxItems = 3;
+    [SerializeField] int timeLimit = 180;
     private int itemsCollected = 0;
 
     private GUIStyle style = new GUIStyle();
 
     public void Start()
     {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
         style.fontSize = 20;
         style.normal.textColor = Color.white;
+    }
+
+    private void End()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     void Update()
@@ -20,6 +30,10 @@ public class GameBehavior : MonoBehaviour
         if (Items >= maxItems)
         {
             Win();
+        }
+        if (Time.time > timeLimit)
+        {
+            Lose();
         }
     }
 
@@ -31,16 +45,20 @@ public class GameBehavior : MonoBehaviour
 
     public void Lose()
     {
+        End();
         UnityEngine.SceneManagement.SceneManager.LoadScene("Loss");
     }
 
     public void Win()
     {
+        End();
         UnityEngine.SceneManagement.SceneManager.LoadScene("Win");
     }
 
     public void OnGUI()
     {
-        GUI.Box(new Rect(20, 50, 150, 25), "Treasure found: " + itemsCollected + "/" + maxItems, style);
+        GUI.Box(new Rect(20, 50, 150, 25), "Treasure found: " + itemsCollected + "/" + maxItems
+        + "\nCurrent time: " + (int) Time.time + "/" + timeLimit, style);
+        // GUI.Box(new Rect(20, 50, 150, 25), , style);
     }
 }
